237e3902498bdaf9aeb689fa351e2723d8b6b88e
Jaezer Lima <jaezer@4blue.com.br>
2023-01-13 00:55:07 -0300

    Adicionando Pet novo

---
+++ divulgar/views.py
@@ -0,0 +1,48 @@
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Tag, Raca, Pet
+# Create your views here.
+
+
+@login_required
+def novo_pet(request):
+    if request.method == "GET":
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
+
+    elif request.method == "POST":
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+        pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+        )
+
+        pet.save()
+
+        for tag_id in tags:
+            tag = Tag.objects.get(id=tag_id)
+            pet.tags.add(tag)
+
+        pet.save()
+        tags = Tag.objects.all()
+        racas = Raca.objects.all()
+        messages.add_message(request, constants.SUCCESS, 'Novo pet cadastrado')
+        return render(request, 'novo_pet.html', {'tags': tags, 'racas': racas})
